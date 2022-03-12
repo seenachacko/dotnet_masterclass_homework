75ca14c31d3323bed77f1474de81ad4dcd6fce92
seenachacko <seenachacko119@gmail.com>
2022-03-12 19:29:13 +0100

    add week2 Homework

---
+++ week2/task7/Program.cs
@@ -0,0 +1,98 @@
+﻿
+var signaler = new Signaler();
+signaler.AddTime(new JupiterTime(2, 00));
+signaler.AddTime(new JupiterTime(4, 00));
+signaler.AddTime(new JupiterTime(6, 00));
+//signaler.Inform();
+
+// We woke up at 4:21
+signaler.Check(new JupiterTime(6, 21));
+
+class JupiterTime
+{
+    private int _hours = 0, _minutes = 0;
+    public int Hours
+    {
+        get
+        {
+            return _hours;
+        }
+        set
+        {
+            if (value < 0) throw new Exception("negative number not acceptable");
+            else if (value > 9) _hours = (value % 10);
+            else _hours = value;
+        }
+    }
+    public int Minutes
+    {
+        get
+        {
+            return _minutes;
+        }
+        set
+        {
+            if (value < 0) throw new Exception("time should be positive number");
+            else if (value > 60)
+            {
+                _hours = _hours + (value / 60);
+                if (_hours > 9) _hours = _hours - 10;
+                _minutes = (value % 60);
+            }
+            else _minutes = value;
+        }
+    }
+
+    public JupiterTime(int hours, int minutes)
+    {
+
+        Hours = hours;
+        Minutes = minutes;
+    }
+}
+
+class Signaler
+{
+    List<JupiterTime> SinglerTimeList = new List<JupiterTime>();
+
+
+    public void AddTime(JupiterTime time)
+
+    {
+        SinglerTimeList.Add(time);
+    }
+    
+
+    public void Inform()
+    {
+        if (SinglerTimeList.Count == 0)
+        {
+            Console.WriteLine("No timers added yet");
+        }
+        else
+        {
+            foreach (var time in SinglerTimeList)
+            {
+                Console.WriteLine($"({time.Hours}:{time.Minutes})");
+            }
+        }
+    }
+    public void Check(JupiterTime time)
+    {
+        foreach (var item in SinglerTimeList)
+        {
+            if (time.Hours > item.Hours  || (time.Hours == item.Hours && time.Minutes > item.Minutes))
+            {
+                  Console.WriteLine($"{item.Hours}:{item.Minutes}");
+            }else
+            {
+                 Console.WriteLine("No signals needed to be sent yet");
+            }
+            
+        }
+
+    }
+    
+
+
+}
---
+++ week2/task8/Program.cs
@@ -0,0 +1,38 @@
+﻿var time1 = new TitanTime(1000, 40);
+var time2 = new TitanTime(11, 7);
+var time3 = new TitanTime(31,70);
+var time4 = new TitanTime(9, 11);
+var time5 = new TitanTime(0, 0);
+
+Console.WriteLine(time1.ToString());
+Console.WriteLine(time2.ToString());
+Console.WriteLine(time3.ToString());
+Console.WriteLine(time4.ToString());
+Console.WriteLine(time5.ToString());
+
+
+class TitanTime
+{
+
+    public int Hour;
+    public int Minutes;
+    public TitanTime(int hour, int minutes)
+    {
+
+        if (hour < 0)
+        {
+            Console.WriteLine("should be grater than 0");
+
+        }
+        else
+        {
+            Hour = hour % 900 + minutes / 60;
+            Minutes = minutes % 60;
+        }
+
+    }
+    public override string ToString()
+    {
+        return $"{Hour:000}:{Minutes:00}";
+    }
+}
