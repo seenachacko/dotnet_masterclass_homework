8726da0b89a9a757351214eb14fa091a97a6f37b
seenachacko <seenachacko119@gmail.com>
2022-03-15 17:41:05 +0100

    add week3 HW

---
+++ week3/webApplicationHw/Models/Distance.cs
@@ -0,0 +1,8 @@
+namespace webApplicationHw.Models;
+public class Distance{
+
+
+    public double Miles { get; set; }
+
+    public double Kilometers { get; set; }
+}
---
+++ week3/webApplicationHw/Models/ValueType.cs
@@ -0,0 +1,9 @@
+
+// namespace webApplicationHw.Models;
+// public enum ValueTypeInternal
+//     {
+//         Miles,
+//         Kilometeres,
+//         Gallons,
+//         Liters
+//     }
